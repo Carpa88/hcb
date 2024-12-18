ef043bb567afc942f50a392788db3d1ac834c596
Carpa88 <olenka_311@mail.ru>
2024-12-18 10:30:49 +0100

    added tamplate for the main

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,28 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import Nav from './ui/Nav';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html className="h-full bg-gray-100" lang="en">
+      <body className={`${inter.className} antialiased h-full`}>
+        <div className="min-h-full bg-red">
+          <Nav />
+          <header className="bg-white shadow">
+          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+          </div>
+        </header>
+        <main>
+          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">{children}</div>
+        </main>
+            
+        </div>  
+      </body>
     </html>
   );
 }
---
+++ app/lib/consts.ts
@@ -0,0 +1,18 @@
+export const user = {
+  name: 'Tom Cook',
+  email: 'tom@example.com',
+  imageUrl:
+    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+}
+export const navigation = [
+  { name: 'Пастухи Беларуси', href: '#', current: true },
+  { name: 'Гостиница для собак', href: '#', current: false },
+  { name: 'Тренировки', href: '#', current: false },
+  { name: 'Соревнования', href: '#', current: false },
+  { name: 'Уроки', href: '#', current: false },
+]
+export const userNavigation = [
+  { name: 'Your Profile', href: '#' },
+  { name: 'Settings', href: '#' },
+  { name: 'Sign out', href: '#' },
+]
