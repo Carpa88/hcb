38c8ca4c6feaed904dea6c36598fe0ad76e4aa82
Carpa88 <olenka_311@mail.ru>
2024-12-24 22:29:18 +0100

    eded styles for title

--- app/ui/Title.tsx
+++ app/ui/Title.tsx
@@ -1,11 +1,13 @@
+import { lusitana } from "./fonts"
+
 const Title = ({title}: {title: String}) => {
   return (
     <header className="bg-white shadow">
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold tracking-tight text-gray-900">{title}</h1>
+        <h1 className={`${lusitana.className} tracking-wider text-3xl font-bold tracking-tight text-gray-900`}>{title}</h1>
       </div>
     </header>
   )
 }
 
-export default Title
+export default Title;
